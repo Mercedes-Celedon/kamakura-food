f6833dad9ff3c10d7fd0ad20eda3c923cde806b3
IsaLagu <isabelafonso321@hotmail.com>
2024-06-06 12:09:14 +0100

    add products parameter to showProducts fn

--- src/events.js
+++ src/events.js
@@ -1,5 +1,6 @@
 //Intenta separar los eventos en este archivo.
 
+import { products } from "../assets/data/data";
 import { showProducts } from "./menu";
 
-document.addEventListener("DOMContentLoaded", showProducts);
+document.addEventListener("DOMContentLoaded", () => showProducts(products));
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,8 @@
-import { products } from "../assets/data/data";
 //DEBE imprimir en pantalla la información de filtros.
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
 
-export function showProducts() {
+export function showProducts(products) {
   let productsHTML = "";
   products.forEach((product) => {
     productsHTML = `${productsHTML}
